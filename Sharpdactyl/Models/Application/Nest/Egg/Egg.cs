be58d9502fda9d11d126feefc93d5ec39cd15ae6
Jordan Wren <jordanmwren@gmail.com>
2021-07-08 06:40:14 -0600

    Created Nest and Egg models

---
+++ Sharpdactyl/Models/Application/Nest/Egg/Egg.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace SharpdactylLib.Models.Application.Nest.Egg
+{
+    /// <summary>Represents the eggs installed inside a nest</summary>
+    public class Egg
+    {
+        public EggDatum[] Data { get; set; }
+    }
+
+    /// <summary>The egg data</summary>
+    public class EggDatum
+    { 
+        public Attributes Attributes { get; set; }
+    }
+
+    /// <summary>The egg attributes</summary>
+    public class Attributes
+    { 
+        /// <summary>The egg id</summary>
+        [JsonProperty("id", Required = Required.Always)]
+        public int Id { get; set; }
+        /// <summary>The egg uuid</summary>
+        [JsonProperty("uuid", Required = Required.Always)]
+        public Guid Uuid { get; set; }
+        /// <summary>The egg name</summary>
+        [JsonProperty("name", Required = Required.Always)]
+        public string Name { get; set; }
+        /// <summary>The egg nest</summary>
+        [JsonProperty("nest", Required = Required.Always)]
+        public int Nest { get; set; }
+        /// <summary>The egg author</summary>
+        [JsonProperty("author", Required = Required.Always)]
+        public string Author { get; set; }
+        /// <summary>The egg description</summary>
+        [JsonProperty("description", Required = Required.Always)]
+        public string Description { get; set; }
+        /// <summary>The egg docker image</summary>
+        [JsonProperty("docker_image", Required = Required.Always)]
+        public string DockerImage { get; set; }
+        /// <summary>The egg startup command</summary>
+        [JsonProperty("startup", Required = Required.Always)]
+        public string Startup { get; set; }
+        /// <summary>The egg creation time</summary>
+        [JsonProperty("created_at", Required = Required.Always)]
+        public DateTimeOffset CreatedAt { get; set; }
+        /// <summary>The egg last updated time</summary>
+        [JsonProperty("updated_at", Required = Required.Always)]
+        public DateTimeOffset UpdatedAt { get; set; }
+    }
+}
---
+++ Sharpdactyl/Models/Application/Nest/Nest.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace SharpdactylLib.Models.Application.Nest
+{
+    /// <summary>Represents the installed nests on the panel</summary>
+    public class Nest
+    {
+        public NestDatum[] Data { get; set; }
+    }
+
+    public class NestDatum
+    {
+        public Attributes Attributes { get; set; }
+    }
+
+    public class Attributes
+    {
+        /// <summary>The nest id</summary>
+        [JsonProperty("id", Required = Required.Always)]
+        public int Id { get; set; }
+        /// <summary>The nest uuid</summary>
+        [JsonProperty("uuid", Required = Required.Always)]
+        public Guid Uuid { get; set; }
+        /// <summary>The nest author</summary>
+        [JsonProperty("author", Required = Required.Always)]
+        public string Author { get; set; }
+        /// <summary>The nest name</summary>
+        [JsonProperty("name", Required = Required.Always)]
+        public string Name { get; set; }
+        /// <summary>The nest description</summary>
+        [JsonProperty("description", Required = Required.Always)]
+        public string Description { get; set; }
+        /// <summary>The nest creation time</summary>
+        [JsonProperty("created_at", Required = Required.Always)]
+        public DateTimeOffset CreatedAt { get; set; }
+        /// <summary>The nest last updated time</summary>
+        [JsonProperty("updated_at", Required = Required.Always)]
+        public DateTimeOffset UpdatedAt { get; set; }
+    }
+}
