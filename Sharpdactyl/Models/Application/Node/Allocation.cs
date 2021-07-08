72e0b365f136d1997eae013b709240fe9cf71ca2
Jordan Wren <jordanmwren@gmail.com>
2021-07-08 05:44:24 -0600

    Created Allocation model

---
+++ Sharpdactyl/Models/Application/Node/Allocation.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace SharpdactylLib.Models.Application.Node
+{
+    /// <summary>The allocation on the node</summary>
+    public class Allocation
+    {
+        /// <summary>The allocation data</summary>
+        [JsonProperty("data", Required = Required.Always)]
+        public AllocationDatum[] Data { get; set; }
+    }
+
+    public class AllocationDatum
+    {
+        /// <summary>The attributes</summary>
+        [JsonProperty("attributes", Required = Required.Always)]
+        public Attributes Attributes { get; set; }
+    }
+
+    public class Attributes
+    {
+        /// <summary>The allocation id</summary>
+        [JsonProperty("id", Required = Required.Always)]
+        public int Id { get; set; }
+        /// <summary>The allocation ip</summary>
+        [JsonProperty("ip", Required = Required.Always)]
+        public string Ip { get; set; }
+        /// <summary>The allocation alias</summary>
+        [JsonProperty("alias", Required = Required.AllowNull)]
+        public string Alias { get; set; }
+        /// <summary>The allocation port</summary>
+        [JsonProperty("port", Required = Required.Always)]
+        public int Port { get; set; }
+        /// <summary>The allocation notes</summary>
+        [JsonProperty("notes", Required = Required.AllowNull)]
+        public string Notes { get; set; }
+        /// <summary>Allocation assigned state</summary>
+        [JsonProperty("assigned", Required = Required.Always)]
+        public bool IsAssigned { get; set; }
+    }
+}
