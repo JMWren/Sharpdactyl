5f2185cfc8765f7c9f5e7f0a8902cb3c8864ae74
Jordan Wren <jordanmwren@gmail.com>
2021-07-15 01:31:10 -0600

    Added client account request models

---
+++ Sharpdactyl/Models/Client/Account/Account.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+
+namespace SharpdactylLib.Models.Client.Account
+{
+    /// <summary>
+    /// Represents all the data inside the client account http request
+    /// </summary>
+    public class AccountContainer
+    {
+        /// <summary>The account attributes</summary>
+        public Account Attributes { get; set; }
+    }
+
+    /// <summary>
+    /// Represents a client user account
+    /// </summary>
+    public class Account
+    {
+        /// <summary>The account id</summary>
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        /// <summary>Whether the account has admin privileges</summary>
+        [JsonProperty("admin")]
+        public bool Admin { get; set; }
+        /// <summary>The account username</summary>
+        [JsonProperty("username")]
+        public string Username { get; set; }
+        /// <summary>The account email</summary>
+        [JsonProperty("email")]
+        public string Email { get; set; }
+        /// <summary>The account's first name</summary>
+        [JsonProperty("first_name")]
+        public string FirstName { get; set; }
+        /// <summary>The account's last name</summary>
+        [JsonProperty("last_name")]
+        public string LastName { get; set; }
+        /// <summary>The account's preferred language</summary>
+        [JsonProperty("language")]
+        public string Language { get; set; }
+    }
+
+}
---
+++ Sharpdactyl/Models/Client/Account/TwoFactor.cs
@@ -0,0 +1,39 @@
+﻿
+namespace SharpdactylLib.Models.Client.Account
+{
+    /// <summary>
+    /// Represents the data inside of a two factor http request
+    /// </summary>
+    public class TwoFactorContainer
+    {
+        /// <summary>2fa data</summary>
+        public TwoFactor Data { get; set; }
+    }
+
+    /// <summary>
+    /// Represents the two factor secret properties
+    /// </summary>
+    public class TwoFactor
+    {
+        /// <summary>Link to TOTP QR code to set up 2fa</summary>
+        public string ImageUrlData { get; set; }
+    }
+
+    /// <summary>
+    /// Represents the data inside of a 2fa enable http request
+    /// </summary>
+    public class RecoveryTokensContainer
+    {
+        public RecoveryTokens Attributes { get; set; }
+    }
+
+    /// <summary>
+    /// Represents the collection of recovery tokens for 2fa
+    /// </summary>
+    public class RecoveryTokens
+    {
+        /// <summary>The array of account recovery tokens</summary>
+        public string[] Tokens { get; set; }
+    }
+
+}
