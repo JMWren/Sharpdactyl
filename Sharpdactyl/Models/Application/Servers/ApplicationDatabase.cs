8ab8f029605d8d4cb54cd9a8c3bf040f4ce8fa20
Jordan Wren <jordanmwren@gmail.com>
2021-07-15 01:26:32 -0600

    Added abstract database class and client database model

--- Sharpdactyl/Models/Application/Servers/ApplicationDatabase.cs
+++ Sharpdactyl/Models/Application/Servers/ApplicationDatabase.cs
@@ -1,44 +1,53 @@
 ﻿using Newtonsoft.Json;
 using System;
 
-namespace SharpdactylLib.Models.Application.Servers.Databases
+namespace SharpdactylLib.Models.Application.Servers
 {
-    /// <summary>Represents the databases</summary>
-    public class Database
+    /// <summary>
+    /// Represents the full results of an application database http request
+    /// </summary>
+    public class ApplicationDatabaseContainerResult
     {
         /// <summary>The data</summary>
         [JsonProperty("data", Required = Required.Always)]
-        public DatabaseDatum[] Data { get; set; }
+        public ApplicationDatabaseContainer[] Data { get; set; }
     }
 
-    /// <summary>Represents the databases datum</summary>
-    public class DatabaseDatum
+    /// <summary>
+    /// Represents all the data inside an application database http request
+    /// </summary>
+    public class ApplicationDatabaseContainer
     {
         /// <summary>The database data</summary>
         [JsonProperty("attributes", Required = Required.Always)]
-        public Attributes Attributes { get; set; }
+        public ApplicationDatabase Attributes { get; set; }
     }
 
-    /// <summary>Represents the database attributes</summary>
-    public class Attributes
+    /// <summary>
+    /// Represents the application database attributes
+    /// </summary>
+    public class ApplicationDatabase : Database
     {
         /// <summary>The database id</summary>
+        [JsonProperty("id")]
         public long Id { get; set; }
         /// <summary>The database's assigned server</summary>
+        [JsonProperty("server")]
         public int Server { get; set; }
         /// <summary>The database host</summary>
+        [JsonProperty("host")]
         public int Host { get; set; }
         /// <summary>The database name</summary>
-        public string Database { get; set; }
-        /// <summary>The database username</summary>
-        public string Username { get; set; }
+        [JsonProperty("database")]
+        public string DatabaseName { get; set; }
         /// <summary>The database remote</summary>
+        [JsonProperty("remote")]
         public string Remote { get; set; }
-        /// <summary>The max connections</summary>
-        public int MaxConnections { get; set; }
         /// <summary>The database creation date</summary>
+        [JsonProperty("created_at")]
         public DateTimeOffset CreatedAt { get; set; }
         /// <summary>The database's last updated date</summary>
+        [JsonProperty("updated_at")]
         public DateTimeOffset UpdatedAt { get; set; }
     }
 }
---
+++ Sharpdactyl/Models/Database.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace SharpdactylLib.Models
+{
+    /// <summary>
+    /// Represents the attributes of a database
+    /// </summary>
+    public abstract class Database
+    {
+        /// <summary>The database username</summary>
+        [JsonProperty("username")]
+        public string Username { get; set; }
+        /// <summary>The max connections</summary>
+        [JsonProperty("max_connections")]
+        public int MaxConnections { get; set; }
+    }
+}
