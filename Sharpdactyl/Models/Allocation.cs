aea606e1a39fded0311850c502fe9144e47ef6f2
Jordan Wren <jordanmwren@gmail.com>
2021-07-15 01:19:47 -0600

    Created abstract allocation class and inheriting client allocation model

---
+++ Sharpdactyl/Models/Allocation.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace SharpdactylLib.Models
+{
+    /// <summary>
+    /// Represents the attributes of an allocation
+    /// </summary>
+    public abstract class Allocation
+    {
+        /// <summary>The allocation id</summary>
+        [JsonProperty("id")]
+        public long Id { get; set; }
+        /// <summary>The allocation ip</summary>
+        [JsonProperty("ip")]
+        public string Ip { get; set; }
+        /// <summary>The allocation port</summary>
+        [JsonProperty("port")]
+        public int Port { get; set; }
+        /// <summary>The allocation notes</summary>
+        [JsonProperty("notes")]
+        public string Notes { get; set; }
+    }
+}
